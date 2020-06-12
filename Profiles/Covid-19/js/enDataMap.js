f3b4c6536ec0d37c3192335bc5aa828a9c9fb2db
Mardy <mardy@odc.org>
2020-06-12 17:34:32 +0700

    [TASK] Create map page for Covid-19

---
+++ Profiles/Covid-19/js/enDataMap.js
@@ -0,0 +1,60 @@
+entryMapJsonEn(
+  [
+    {
+      "is_ready": "yes",
+      "title_en": "Access to health facilities",
+      "title_km": "លទ្ធភាពទៅដល់មណ្ឌលសុខភាព",
+      "url_en": "https://data.opendevelopmentcambodia.net/dataset/access-to-health-facilities?type=dataset",
+      "url_km": "https://data.opendevelopmentcambodia.net/km/dataset/access-to-health-facilities?type=dataset",
+      "image_src": "https://data.opendevelopmentmekong.net/dataset/917d8da6-01bc-41d8-8b35-9378759470b5/resource/0883f2bb-24a0-4558-86e9-cf96ed0a27c2/download/access-to-health-facilities.jpg"
+    },
+    {
+      "is_ready": "yes",
+      "title_en": "Basemap of Cambodia (2014)",
+      "title_km": "ផែនទីគោល៖ ប្រទេសកម្ពុជា (២០១៤)",
+      "url_en": "https://data.opendevelopmentcambodia.net/dataset/administrative-boundaries-of-cambodia-2014?type=dataset",
+      "url_km": "https://data.opendevelopmentcambodia.net/km/dataset/administrative-boundaries-of-cambodia-2014?type=dataset",
+      "image_src": "https://data.opendevelopmentmekong.net/dataset/7ffe6a04-3f20-40ca-80e7-21e8561dd294/resource/58192411-bdb2-410a-95bd-d76644deec39/download/basemap_en.jpg"
+    },
+    {
+      "is_ready": "yes",
+      "title_en": "Coronavirus (COVID-19) cases in Cambodia",
+      "title_km": "ករណីវីរុសកូរ៉ូណា (កូវីដ-១៩) នៅក្នុងប្រទេសកម្ពុជា",
+      "url_en": "https://data.opendevelopmentcambodia.net/dataset/coronavirus-covid-19-case-in-cambodia?type=dataset",
+      "url_km": "https://data.opendevelopmentcambodia.net/km/dataset/coronavirus-covid-19-case-in-cambodia?type=dataset",
+      "image_src": "https://opendevelopmentcambodia.net/wp-content/uploads/sites/2/2020/06/covid19_cases.png"
+    },
+    {
+      "is_ready": "yes",
+      "title_en": "Health facilities in Cambodia (2010)",
+      "title_km": "មន្ទីរពេទ្យ៖ ប្រទេសកម្ពុជា(២០១០)",
+      "url_en": "https://data.opendevelopmentcambodia.net/dataset/health-facility-of-cambodia-2010?type=dataset",
+      "url_km": "https://data.opendevelopmentcambodia.net/km/dataset/health-facility-of-cambodia-2010?type=dataset",
+      "image_src": "https://data.opendevelopmentmekong.net/dataset/801eed84-77a8-43de-b4a6-8aafd02ebd1f/resource/48d28314-1230-4c7f-a9c6-85e5142fcbfa/download/health-facililty.jpeg"
+    },
+    {
+      "is_ready": "yes",
+      "title_en": "Health facilities signed agreement for health care scheme (NSSF)",
+      "title_km": "​មូលដ្ឋាន​សុខាភិបាល​ដែល​បាន​ចុះ​កិច្ចព្រមព្រៀង​សម្រាប់​របប​ថែទាំ​សុខភាព​ (​ប​.​ស​.​ស​)",
+      "url_en": "https://data.opendevelopmentcambodia.net/dataset/health-facilities-signed-agreement-for-health-care-scheme-nssf?type=dataset",
+      "url_km": "https://data.opendevelopmentcambodia.net/km/dataset/health-facilities-signed-agreement-for-health-care-scheme-nssf?type=dataset",
+      "image_src": "https://data.opendevelopmentmekong.net/dataset/88d09447-c11d-4fc8-82e6-25a18f0057ee/resource/4dec2f8f-2774-460a-96a5-f15a8b939f0c/download/nssf_healthcare.jpeg"
+    },
+    {
+      "is_ready": "yes",
+      "title_en": "Health facilities​ signed agreement for occupational risk scheme (NSSF)",
+      "title_km": "មូល​ដ្ឋាន​សុខាភិបាល​ដែល​បាន​ចុះ​កិច្ច​ព្រមព្រៀង​សម្រាប់​ហានិភ័យ​ការងារ​ (​ប​.​ស​.​ស​)",
+      "url_en": "https://data.opendevelopmentcambodia.net/dataset/health-facilities-nssf?type=dataset",
+      "url_km": "https://data.opendevelopmentcambodia.net/km/dataset/health-facilities-nssf?type=dataset",
+      "image_src": "https://data.opendevelopmentmekong.net/dataset/fa35acac-2fdc-4c64-bd59-7b9be7c48cae/resource/56b51721-209d-4dd0-aad0-8e1af45b9129/download/nssf_en.jpeg"
+    },
+    {
+      "is_ready": "yes",
+      "title_en": "Reserved hospitals for COVID-19 in capital city and provinces in Cambodia",
+      "title_km": "​ទីតាំង​បម្រុង​ធ្វើ​ជា​មន្ទីរពេទ្យ​សម្រាប់​ព្យាបាល​ជម្ងឺ​កូ​វី​ដ​១៩​ នៅ​តាម​រាជធានី​-​ខេត្ត​ទូ​ទាំង​ប្រទេស​",
+      "url_en": "https://data.opendevelopmentcambodia.net/dataset/reserved-hospitals-for-covid-19-by-capital-provinces-in-cambodia?type=dataset",
+      "url_km": "https://data.opendevelopmentcambodia.net/km/dataset/reserved-hospitals-for-covid-19-by-capital-provinces-in-cambodia?type=dataset",
+      "image_src": "https://data.opendevelopmentcambodia.net/en/dataset/485f6459-b23c-4190-9d53-6c8720ee6661/resource/9ffccc8b-6008-4fc4-8bb8-d305f1dc7aae/download/reserved_hospitals.jpg"
+    }
+  ]
+)
